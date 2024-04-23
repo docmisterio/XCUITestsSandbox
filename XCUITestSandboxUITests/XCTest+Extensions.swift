4a154156f7a99cbddd9e4ee87f9864e08285d6e4
Zach Becker <doc.misterio@gmail.com>
2024-04-23 16:32:47 -0500

    moved app.launch() up to the setup, hooked up the first test testLabelShowsTextFieldInput() to the extensions for ease of use - I'll add test explainers next time as well

--- XCUITestSandboxUITests/XCTest+Extensions.swift
+++ XCUITestSandboxUITests/XCTest+Extensions.swift
@@ -1,38 +1,37 @@
 import XCTest
-import Foundation
-
-/// Best to collect like items in enums for use later, bonus points for good naming so everything is clear. 
+/// Best to collect like items in enums for use later, bonus points for good naming so everything is clear.
 enum waitTimeIntervals {
     static let standard: TimeInterval = 2
     static let extended: TimeInterval = 5
 }
 
+enum typedText {
+    static let test = "test"
+}
+
 enum Errors {
+    static let couldNotFindElement = "Element Could Not Be Found"
     static let couldNotTapOnElement = "Element Could Not Be Tapped On"
 }
 
-extension XCTestCase {
-    
-    enum waitTimeIntervals {
-        static let standard = 2
-        static let extended = 5
-    }
-    
+extension XCTestCase {    
     /// we never wanna use this but if we have to use this it's here
     func sleep(forSeconds numberOfSeconds: TimeInterval) {
         Thread.sleep(forTimeInterval: numberOfSeconds)
     }
     
-    func waitForExistance(ofElement element: XCUIElement, forSeconds numberOfSeconds: TimeInterval) -> Bool {
-        return element.waitForExistence(timeout: numberOfSeconds)
+    func findElement(_ element: XCUIElement, timeOutAt numberOfSeconds: TimeInterval, andTap tap: Bool? = nil) throws {
+        guard element.waitForExistence(timeout: 2) else {
+            XCTFail(Errors.couldNotFindElement)
+            return
+        }
+        if tap == true {
+            element.tap()
+        }
     }
     
-    func waitForExistanceAndTap(ofElement element: XCUIElement, forSeconds numberOfSeconds: TimeInterval, andTap: Bool?) {
-        if element.waitForExistence(timeout: 2) {
-            element.tap()
-        } else {
-            XCTFail(Errors.couldNotTapOnElement)
-        }
+    func verifyElement(_ element: XCUIElement) {
+        XCTAssertTrue(element.exists)
     }
 }
 
