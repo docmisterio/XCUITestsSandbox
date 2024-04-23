22962140d70235d9ad2b71c5b4c66a74200227c8
Zach Becker <doc.misterio@gmail.com>
2024-04-23 17:19:05 -0500

    added some slider variables, some progress variables and renamed Errors to Failures

--- XCUITestSandboxUITests/XCTest+Extensions.swift
+++ XCUITestSandboxUITests/XCTest+Extensions.swift
@@ -9,9 +9,21 @@ enum typedText {
     static let test = "test"
 }
 
-enum Errors {
+enum Failures {
     static let couldNotFindElement = "Element Could Not Be Found"
     static let couldNotTapOnElement = "Element Could Not Be Tapped On"
+    static let couldNotConvertPercentageToString = "Could Not Convert Percentage to String"
+}
+
+enum SliderPositions {
+    static let one: CGFloat = 1
+    static let zero: CGFloat = 0
+}
+
+enum ProgressBarPercentages {
+    static let OneHundredPercent: String = "100%"
+    static let zeroPercent: String = "0%"
+
 }
 
 extension XCTestCase {    
@@ -22,7 +34,7 @@ extension XCTestCase {
     
     func findElement(_ element: XCUIElement, timeOutAt numberOfSeconds: TimeInterval, andTap tap: Bool? = nil) throws {
         guard element.waitForExistence(timeout: 2) else {
-            XCTFail(Errors.couldNotFindElement)
+            XCTFail(Failures.couldNotFindElement)
             return
         }
         if tap == true {
