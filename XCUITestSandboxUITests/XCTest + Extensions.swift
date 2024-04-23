d1f4ee9ccca87e87324a0745a04fe6cf5993ce48
Zach Becker <doc.misterio@gmail.com>
2024-04-23 15:26:53 -0500

    Added XCTestCase extensions file with waitTimeIntervals and Errors enums and clarifying / ease of use extensions like sleep, waitForExistance, and waitForExistanceAndTap. That should be good for now but we'll see if I need more

--- XCUITestSandboxUITests/XCTest + Extensions.swift
+++ XCUITestSandboxUITests/XCTest + Extensions.swift
@@ -1,9 +1,41 @@
-//
-//  XCTest + Extensions.swift
-//  XCUITestSandboxUITests
-//
-//  Created by Zach Becker on 4/23/24.
-//  Copyright © 2024 Hacking with Swift. All rights reserved.
-//
-
+import XCTest
 import Foundation
+
+/// Best to collect like items in enums for use later, bonus points for good naming so everything is clear. 
+enum waitTimeIntervals {
+    static let standard: TimeInterval = 2
+    static let extended: TimeInterval = 5
+}
+
+enum Errors {
+    static let couldNotTapOnElement = "Element Could Not Be Tapped On"
+}
+
+extension XCTestCase {
+    
+    enum waitTimeIntervals {
+        static let standard = 2
+        static let extended = 5
+    }
+    
+    /// we never wanna use this but if we have to use this it's here
+    func sleep(forSeconds numberOfSeconds: TimeInterval) {
+        Thread.sleep(forTimeInterval: numberOfSeconds)
+    }
+    
+    func waitForExistance(ofElement element: XCUIElement, forSeconds numberOfSeconds: TimeInterval) -> Bool {
+        return element.waitForExistence(timeout: numberOfSeconds)
+    }
+    
+    func waitForExistanceAndTap(ofElement element: XCUIElement, forSeconds numberOfSeconds: TimeInterval, andTap: Bool?) {
+        if element.waitForExistence(timeout: 2) {
+            element.tap()
+        } else {
+            XCTFail(Errors.couldNotTapOnElement)
+        }
+    }
+}
+
+
+
+
